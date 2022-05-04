9c1d0afcf2633d1953ce30289974cd6e461b7217
PraveenReddy I <narayana.praveen143@gmail.com>
2022-05-04 13:29:38 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,14 +1,4 @@
-resource "aws_s3_bucket" "onebucket" {
-   bucket = "delete-100"
-   acl = "private"
-   versioning {
-      enabled = true
-   }
-   tags = {
-     Name = "Bucket1"
-     Environment = "Test"
-   }
-}
+
 terraform {
 
     cloud {
