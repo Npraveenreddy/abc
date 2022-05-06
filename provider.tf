2f98a3bec2bd1311645adc803fa3befb8150f3cb
PraveenReddy I <narayana.praveen143@gmail.com>
2022-05-06 12:35:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,2 +1,4 @@
-  AWS_ACCESS_KEY_ID = var.AWS_ACCESS_KEY_ID
-  AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY
+provider "aws" {
+    region                = "us-east-2"
+    shared_credentials_file = "~/.aws/credentials"
+}
