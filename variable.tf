e38238fb0fc7ccc9c17d67f828b1d6885d3a7053
PraveenReddy I <narayana.praveen143@gmail.com>
2022-05-06 11:01:37 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-2"
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  type = number
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = number
+}
